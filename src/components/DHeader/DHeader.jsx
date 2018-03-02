9da31fb88683274a942c9e06cd4e478bc72bef0a
Daniel Evans <migetman9@gmail.com>
2018-03-02 02:16:55 -0800

    Switch to toggle for copy visit

--- src/components/DHeader/DHeader.jsx
+++ src/components/DHeader/DHeader.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
 import { Layout } from 'antd';
-import {Button } from 'react-md';
+import {Button, Switch } from 'react-md';
 const { Header } = Layout;
 import config from "../../../data/SiteConfig";
 import copy from 'copy-to-clipboard';
@@ -15,6 +15,7 @@ class DHeader extends Component{
     <Header className="header">
     <Button icon className="icon" iconClassName="fa fa-bars" onClick={this.props.popSider}></Button>
     Redirection
+    <Switch className="switch" id="copy-switch" type="switch" labelBefore={true} label={ this.props.copy ? "Copy URL" : "Visit URL"} name="copy" defaultChecked={this.props.copy} onChange={this.props.toggleCopy}/>
     </Header>
     );
     }
